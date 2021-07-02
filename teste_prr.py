06be22212c11b3a4c109a71e9b3a3195f8a8a042
Marcus Holanda <mvsh777@hotmail.com>
2021-07-02 15:14:26 -0300

    delete de arquivos

--- teste_prr.py
+++
@@ -1,9 +0,0 @@
-import re
-
-
-
-compilador = re.compile(r'\d.*,\d{2}')
-ret = compilador.search('R$ \n\n\n116,92 \n\n\n\n '
-                        ''
-                        'a vista')
-print(ret.group())
